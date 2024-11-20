61672e8a89d796c14007b34eebb96eeaad7cde84
moesaid <masedup@gmail.com>
2024-11-19 20:41:07 -0500

    Update license_parser_test.dart

--- test/license_parser_test.dart
+++ test/license_parser_test.dart
@@ -7,16 +7,68 @@ void main() {
   group("Parsing Versions of the AAMVA DL/ID Card Design Standard", () {
     group("Parsed License", () {
       test("should expose the raw pdf417 data", () {
-        final rawData =
-            "@\n\nANSI 636026010102DL00410288ZA03290015DLDBJD12345678\nDAAPUBLIC,JOHN,Q\nDABPUBLIC\nDACJOHN\nDADQUINCY\nDAG789 E OAK ST\nDAIANYTOWN\nDAHAPT #4A\nDAJCA\nDAK90223\nDBB19700115\nDBA20350131\nDAU509\nDAW180\nDAZBR\nDAYGRN\nDBC1\nDBHN\nDARD\nDBD20131003\nDBE2013-10-03 00.00.00.000000\nDBIN\nZAZAA7V81\n";
+        final rawData = "@\n" +
+            "\n" +
+            "ANSI 636026010102DL00410288ZA03290015DLDBJD12345678\n" +
+            "DAAPUBLIC,JOHN,Q\n" +
+            "DABPUBLIC\n" +
+            "DACJOHN\n" +
+            "DADQUINCY\n" +
+            "DAG789 E OAK ST\n" +
+            "DAIANYTOWN\n" +
+            "DAHAPT #4A\n" +
+            "DAJCA\n" +
+            "DAK90223\n" +
+            "DBB19700115\n" +
+            "DBA20350131\n" +
+            "DAU509\n" +
+            "DAW180\n" +
+            "DAZBR\n" +
+            "DAYGRN\n" +
+            "DBC1\n" +
+            "DBHN\n" +
+            "DARD\n" +
+            "DBD20131003\n" +
+            "DBE2013-10-03 00.00.00.000000\n" +
+            "DBIN\n" +
+            "ZAZAA7V81\n";
 
         final result = LicenseParser.parse(rawData);
         expect(result.pdf417, rawData);
       });
 
       test("parses my license", () {
+<<<<<<< HEAD
         final rawData =
             "@\n\nANSI 636001030302DL00410279ZN03200070DLDCAD\nDCBNONE\nDCDNONE\nDBA10262023\nDCSMUNOZ\nDCTROSALIA\nDBD11032018\nDBB10261992\nDBC2\nDAYBRO\nDAU502\nDAG2400 PACIFIC ST 1\nDAIBROOKLYN\nDAJNY\nDAK112330000\nDAQ762877257\nDCFZ6CYT5M618\nDCGUSA\nDDAN\nDDB09282017\nDDD0\nZNZNAMUNOZ@ROSALIA\nZNBb6Fj:%5VgcG<Z2<>cfn6H'p+th./8g@e!99!:eh";
+=======
+        final rawData = "@\n" +
+        "\n" +
+        "ANSI 636001030302DL00410279ZN03200070DLDCAD\n" +
+        "DCBNONE\n" +
+        "DCDNONE\n" +
+        "DBA10262023\n" +
+        "DCSMUNOZ\n" +
+        "DCTROSALIA\n" +
+        "DBD11032018\n" +
+        "DBB10261992\n" +
+        "DBC2\n" +
+        "DAYBRO\n" +
+        "DAU502\n" +
+        "DAG2400 PACIFIC ST 1\n" +
+        "DAIBROOKLYN\n" +
+        "DAJNY\n" +
+        "DAK112330000\n" +
+        "DAQ762877257\n" +
+        "DCFZ6CYT5M618\n" +
+        "DCGUSA\n" +
+        "DDAN\n" +
+        "DDB09282017\n" +
+        "DDD0\n" +
+        "ZNZNAMUNOZ@ROSALIA\n" +
+        // ignore: unnecessary_string_escapes
+        "ZNBb6Fj:%5VgcG<Z2<>cfn6H'p+th./8g@e!99!:e\h";
+>>>>>>> parent of 645e8e7 (Update license_parser_test.dart)
 
         final result = LicenseParser.parse(rawData);
         expect(result.pdf417, rawData);
