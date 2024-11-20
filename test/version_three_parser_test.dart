1c676f861ab860a325e822d43026708f16b090a5
moesaid <masedup@gmail.com>
2024-11-19 20:39:00 -0500

    Revert "Update version_three_parser_test.dart"
    This reverts commit 4e77ea2f746807fc5d86083521824a0b448e702a.

--- test/version_three_parser_test.dart
+++ test/version_three_parser_test.dart
@@ -8,8 +8,28 @@ import 'package:test/test.dart';
 void main() {
   group("Parsing version three of the AAMVA standard", () {
     test("should parse the required fields", () {
-      final rawData =
-          "@\n\nANSI 636012030001DL00000367DLDCAG2  \nDCBX\nDCDNONE\nDBA04252025\nDCSPUBLIC,\nDCTJOHN\nDBD09102015\nDBB03281974\nDBC1\nDAYNONE\nDAU170 cm\nDAG789 E OAK AVE,\nDAIVANCOUVER\nDAJBC\nDAKA5K 4X9\nDAQM1234-12345-12345\nDCFAB1234567\nDCGCAN\nDCHNONE\nDCK*1234567*";
+      final rawData = "@\n" +
+          "\n" +
+          "ANSI 636012030001DL00000367DLDCAG2  \n" +
+          "DCBX\n" +
+          "DCDNONE\n" +
+          "DBA04252025\n" +
+          "DCSPUBLIC,\n" +
+          "DCTJOHN\n" +
+          "DBD09102015\n" +
+          "DBB03281974\n" +
+          "DBC1\n" +
+          "DAYNONE\n" +
+          "DAU170 cm\n" +
+          "DAG789 E OAK AVE,\n" +
+          "DAIVANCOUVER\n" +
+          "DAJBC\n" +
+          "DAKA5K 4X9\n" +
+          "DAQM1234-12345-12345\n" +
+          "DCFAB1234567\n" +
+          "DCGCAN\n" +
+          "DCHNONE\n" +
+          "DCK*1234567*";
 
       final result = LicenseParser.parse(rawData);
 
@@ -17,7 +37,7 @@ void main() {
       expect(result.firstName, "JOHN");
       expect(result.lastName, "PUBLIC");
       expect(result.streetAddress, "789 E OAK AVE");
-      expect(result.postalCode, PostalCode(postalCode: "A5K 4X9")); //"A5K 4X9"
+      expect(result.postalCode, PostalCode(postalCode: "A5K 4X9"));//"A5K 4X9"
       //"20250425"
       expect(result.expirationDate, DateTime(2025, 4, 25));
       //"19740328"
